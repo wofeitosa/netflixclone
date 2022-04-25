601e97bbbb93b1f9c368f1197613bbc72fe80423
Wagner Feitosa <wofeitosa@yahoo.com.br>
2022-04-25 19:41:56 -0300

    criado index,css,js e modificado o projeto

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Row from './components/Row';
-import requests from './request'
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
-function App() {
-  return (
-    <div className="app">
+export default () => {
 
-    <h1>Netflix clone</h1>
-    <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
-    </div>
-  );
+  useEffect(()=>{
+    const loadAll = async () => {
+      // pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      //Pegando o Featured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+
+      
+
+      
+  }
+
+  loadAll();
+}) ;
+
+return (
+  <div className='page'>
+
+    {featuredData &&
+      <FeaturedMovie  item={featuredData} />    
+
+    
+    }
+
+
+    <section className='lists'>
+      {movieList.map((item, key)=>(
+       <MovieRow key={key} title={item.title} items= {item.items}/>
+
+      ))}   
+    </section>
+    
+  </div>
+);
 }
 
 
-export default App;
+      
 
- 
-
+    
   
+
+
+    
---
+++ src/components/FeaturedMovie.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './FeaturedMovie.css';
+
+export default ({item}) => {
+    return (
+        <section className="featured" style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+        }}>
+            <div className="featured--vertical">
+                
+            </div>
+        </section>
+    )
+}
+
+
+
+
